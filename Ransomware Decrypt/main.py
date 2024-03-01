2c69c08ffd179d3c13f1ce7baf8f9dac35bcdd05
Enes Koç <kenes7667@gmail.com>
2024-03-01 19:02:42 +0300

    Verilen şifre kontrolü

--- Ransomware Decrypt/main.py
+++ Ransomware Decrypt/main.py
@@ -1,23 +1,29 @@
 import os
 from cryptography.fernet import Fernet
 
-
-file_list = []
-
-for file in os.listdir():
-    if file == "ransom.py" or file == "generatedkey.key" or file == "main.py":
-        continue
-    if os.path.isfile(file):
-        file_list.append(file)
+user_input_key = input("Enter the key: ")
 
 
-with open("generatedkey.key","rb") as generatedkey:
+try:
+    Fernet(user_input_key)
+except Exception as e:
+    print("Invalid key. Exiting...")
+    exit()
+
+file_list = [file for file in os.listdir() if file not in ["ransom.py", "generatedkey.key", "main.py"] and os.path.isfile(file)]
+
+with open("generatedkey.key", "rb") as generatedkey:
     secretkey = generatedkey.read()
 
+cipher = Fernet(user_input_key)
 
 for file in file_list:
-    with open(file,"rb") as the_file:
+    with open(file, "rb") as the_file:
         contents = the_file.read()
-    contents_decrypted = Fernet(key).decrypt(contents)
-    with open(file,"wb") as the_file:
-        the_file.write(contents_decrypted)
+    try:
+        contents_decrypted = cipher.decrypt(contents)
+        with open(file, "wb") as the_file:
+            the_file.write(contents_decrypted)
+        print(f"{file} decrypted successfully.")
+    except Exception as e:
+        print(f"Error decrypting {file}: {e}")
